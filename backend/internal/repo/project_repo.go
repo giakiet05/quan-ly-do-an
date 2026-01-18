fc6bf8e387b2ce339c588b8f46f1e3a90e0ae09d
VoAnKhoi2005 <voankhoi@gmail.com>
2026-01-18 14:57:51 +0700

    feat: finish project service

--- backend/internal/repo/project_repo.go
+++ backend/internal/repo/project_repo.go
@@ -2,6 +2,7 @@ package repo
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/giakiet05/quan-ly-do-an/backend/internal/apperror"
@@ -15,17 +16,25 @@ import (
 
 type ProjectRepo interface {
 	CreateProjectRound(ctx context.Context, classroomID string, round *model.ProjectRound) error
-	GetProjectRound(ctx context.Context, classroomID, roundID string) (*model.ProjectRound, error)
+	CreateProjectRounds(ctx context.Context, classroomID string, rounds []model.ProjectRound) error
+	GetProjectRoundByID(ctx context.Context, classroomID, roundID string) (*model.ProjectRound, error)
+	GetProjectRoundsByClassroomID(ctx context.Context, classroomID string) ([]model.ProjectRound, error)
 	ListProjectRounds(ctx context.Context, classroomID string, page, pageSize int) ([]model.ProjectRound, int64, error)
+	DeleteProjectRound(ctx context.Context, classroomID, roundID string) error
 
 	CreateProject(ctx context.Context, project *model.Project) error
 	CreateProjects(ctx context.Context, projects []model.Project) error
+	GetProjectByID(ctx context.Context, classroomID, projectID string) (*model.Project, error)
+	GetProjectsByRoundID(ctx context.Context, classroomID, roundID string) ([]model.Project, error)
 	ReplaceProject(ctx context.Context, project *model.Project) error
-	DeleteProject(ctx context.Context, projectID string) error
+	DeleteProject(ctx context.Context, classroomID, projectID string) error
 
 	CreateReportPeriod(ctx context.Context, classroomID, roundID string, period *model.ReportPeriod) error
+	CreateReportPeriods(ctx context.Context, classroomID, roundID string, reportPeriods []model.ReportPeriod) error
+	GetReportPeriodByID(ctx context.Context, classroomID, roundID, reportPeriodID string) (*model.ReportPeriod, error)
+	GetReportPeriodsByRoundID(ctx context.Context, classroomID, roundID string) ([]model.ReportPeriod, error)
 	ReplaceReportPeriod(ctx context.Context, reportPeriod *model.ReportPeriod) error
-	DeleteReportPeriod(ctx context.Context, classroomID, roundID, reportPeriodID string) error
+	DeleteReportPeriod(ctx context.Context, classroomID string, roundID string, reportPeriodID string) error
 }
 
 type projectRepo struct {
@@ -52,7 +61,49 @@ func (p *projectRepo) CreateProjectRound(ctx context.Context, classroomID string
 	return err
 }
 
-func (p *projectRepo) GetProjectRound(ctx context.Context, classroomID, roundID string) (*model.ProjectRound, error) {
+func (p *projectRepo) CreateProjectRounds(
+	ctx context.Context,
+	classroomID string,
+	rounds []model.ProjectRound,
+) error {
+
+	if len(rounds) == 0 {
+		return nil
+	}
+
+	classroomObjectID, err := primitive.ObjectIDFromHex(classroomID)
+	if err != nil {
+		return err
+	}
+
+	now := time.Now()
+	for i := range rounds {
+		rounds[i].ID = primitive.NewObjectID()
+		rounds[i].CreatedAt = now
+	}
+
+	filter := bson.M{"_id": classroomObjectID}
+	update := bson.M{
+		"$push": bson.M{
+			"rounds": bson.M{
+				"$each": rounds,
+			},
+		},
+	}
+
+	res, err := p.classroomCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return err
+	}
+
+	if res.ModifiedCount == 0 {
+		return apperror.ErrClassroomNotFound
+	}
+
+	return nil
+}
+
+func (p *projectRepo) GetProjectRoundByID(ctx context.Context, classroomID, roundID string) (*model.ProjectRound, error) {
 	classroomObjectID, err := primitive.ObjectIDFromHex(classroomID)
 	if err != nil {
 		return nil, err
@@ -87,6 +138,39 @@ func (p *projectRepo) GetProjectRound(ctx context.Context, classroomID, roundID 
 	}
 	return &round, nil
 }
+
+func (p *projectRepo) GetProjectRoundsByClassroomID(
+	ctx context.Context,
+	classroomID string,
+) ([]model.ProjectRound, error) {
+
+	classroomOID, err := primitive.ObjectIDFromHex(classroomID)
+	if err != nil {
+		return nil, apperror.ErrBadRequest
+	}
+
+	var result struct {
+		Rounds []model.ProjectRound `bson:"rounds"`
+	}
+
+	err = p.classroomCollection.FindOne(
+		ctx,
+		bson.M{"_id": classroomOID},
+		options.FindOne().SetProjection(bson.M{
+			"rounds": 1,
+		}),
+	).Decode(&result)
+
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, apperror.ErrClassroomNotFound
+		}
+		return nil, err
+	}
+
+	return result.Rounds, nil
+}
+
 func (p *projectRepo) ListProjectRounds(ctx context.Context, classroomID string, page, pageSize int) ([]model.ProjectRound, int64, error) {
 	classroomObjectID, err := primitive.ObjectIDFromHex(classroomID)
 	if err != nil {
@@ -140,6 +224,46 @@ func (p *projectRepo) ListProjectRounds(ctx context.Context, classroomID string,
 	return rounds, total, nil
 }
 
+func (p *projectRepo) DeleteProjectRound(
+	ctx context.Context,
+	classroomID, roundID string,
+) error {
+
+	classroomOID, err := primitive.ObjectIDFromHex(classroomID)
+	if err != nil {
+		return apperror.ErrBadRequest
+	}
+
+	roundOID, err := primitive.ObjectIDFromHex(roundID)
+	if err != nil {
+		return apperror.ErrBadRequest
+	}
+
+	filter := bson.M{
+		"_id":        classroomOID,
+		"rounds._id": roundOID,
+	}
+
+	update := bson.M{
+		"$pull": bson.M{
+			"rounds": bson.M{
+				"_id": roundOID,
+			},
+		},
+	}
+
+	res, err := p.classroomCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return err
+	}
+
+	if res.ModifiedCount == 0 {
+		return apperror.ErrRoundNotFound
+	}
+
+	return nil
+}
+
 func (p *projectRepo) CreateProject(ctx context.Context, project *model.Project) error {
 	filter := bson.M{"_id": project.ClassroomID, "rounds._id": project.ProjectRoundID}
 	update := bson.M{"$push": bson.M{"rounds.$.projects": project}}
@@ -170,6 +294,105 @@ func (p *projectRepo) CreateProjects(ctx context.Context, projects []model.Proje
 	return nil
 }
 
+func (p *projectRepo) GetProjectByID(
+	ctx context.Context,
+	classroomID string,
+	projectID string,
+) (*model.Project, error) {
+
+	classroomOID, err := primitive.ObjectIDFromHex(classroomID)
+	if err != nil {
+		return nil, apperror.ErrBadRequest
+	}
+
+	projectOID, err := primitive.ObjectIDFromHex(projectID)
+	if err != nil {
+		return nil, apperror.ErrBadRequest
+	}
+
+	pipeline := mongo.Pipeline{
+		{{Key: "$match", Value: bson.M{
+			"_id": classroomOID,
+		}}},
+		{{Key: "$unwind", Value: "$rounds"}},
+		{{Key: "$unwind", Value: "$rounds.projects"}},
+		{{Key: "$match", Value: bson.M{
+			"rounds.projects._id": projectOID,
+		}}},
+		{{Key: "$replaceRoot", Value: bson.M{
+			"newRoot": "$rounds.projects",
+		}}},
+	}
+
+	cursor, err := p.classroomCollection.Aggregate(ctx, pipeline)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	if !cursor.Next(ctx) {
+		return nil, apperror.ErrProjectNotFound
+	}
+
+	var project model.Project
+	if err := cursor.Decode(&project); err != nil {
+		return nil, err
+	}
+
+	return &project, nil
+}
+
+func (p *projectRepo) GetProjectsByRoundID(
+	ctx context.Context,
+	classroomID, roundID string,
+) ([]model.Project, error) {
+
+	classroomOID, err := primitive.ObjectIDFromHex(classroomID)
+	if err != nil {
+		return nil, apperror.ErrBadRequest
+	}
+
+	roundOID, err := primitive.ObjectIDFromHex(roundID)
+	if err != nil {
+		return nil, apperror.ErrBadRequest
+	}
+
+	var result struct {
+		Rounds []struct {
+			Projects []model.Project `bson:"projects"`
+		} `bson:"rounds"`
+	}
+
+	err = p.classroomCollection.FindOne(
+		ctx,
+		bson.M{"_id": classroomOID},
+		options.FindOne().SetProjection(bson.M{
+			"rounds": bson.M{
+				"$filter": bson.M{
+					"input": "$rounds",
+					"as":    "r",
+					"cond": bson.M{
+						"$eq": []interface{}{"$$r._id", roundOID},
+					},
+				},
+			},
+		}),
+	).Decode(&result)
+
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, apperror.ErrClassroomNotFound
+		}
+		return nil, err
+	}
+
+	if len(result.Rounds) == 0 {
+		return nil, apperror.ErrRoundNotFound
+	}
+
+	return result.Rounds[0].Projects, nil
+}
+
 func (p *projectRepo) ReplaceProject(ctx context.Context, project *model.Project) error {
 	// find any classroom that contains this project
 	filter := bson.M{"rounds.projects._id": project.ID}
@@ -190,23 +413,43 @@ func (p *projectRepo) ReplaceProject(ctx context.Context, project *model.Project
 	return nil
 }
 
-func (p *projectRepo) DeleteProject(ctx context.Context, projectID string) error {
-	projectObjectID, err := primitive.ObjectIDFromHex(projectID)
+func (p *projectRepo) DeleteProject(
+	ctx context.Context,
+	classroomID, projectID string,
+) error {
+
+	classroomOID, err := primitive.ObjectIDFromHex(classroomID)
+	if err != nil {
+		return apperror.ErrBadRequest
+	}
+
+	projectOID, err := primitive.ObjectIDFromHex(projectID)
+	if err != nil {
+		return apperror.ErrBadRequest
+	}
+
+	filter := bson.M{
+		"_id":                 classroomOID,
+		"rounds.projects._id": projectOID,
+	}
+
+	update := bson.M{
+		"$pull": bson.M{
+			"rounds.$[].projects": bson.M{
+				"_id": projectOID,
+			},
+		},
+	}
+
+	res, err := p.classroomCollection.UpdateOne(ctx, filter, update)
 	if err != nil {
 		return err
 	}
 
-	filter := bson.M{"rounds.projects._id": projectObjectID}
-	// remove from all rounds' projects arrays where present
-	update := bson.M{"$pull": bson.M{"rounds.$[].projects": bson.M{"_id": projectObjectID}}}
-
-	res, err := p.classroomCollection.UpdateMany(ctx, filter, update)
-	if err != nil {
-		return err
-	}
 	if res.ModifiedCount == 0 {
 		return apperror.ErrProjectNotFound
 	}
+
 	return nil
 }
 
@@ -231,6 +474,150 @@ func (p *projectRepo) CreateReportPeriod(ctx context.Context, classroomID, round
 		return apperror.ErrRoundNotFound
 	}
 	return nil
+}
+
+func (p *projectRepo) CreateReportPeriods(
+	ctx context.Context,
+	classroomID, roundID string,
+	reportPeriods []model.ReportPeriod,
+) error {
+
+	classroomObjectID, err := primitive.ObjectIDFromHex(classroomID)
+	if err != nil {
+		return err
+	}
+	roundObjectID, err := primitive.ObjectIDFromHex(roundID)
+	if err != nil {
+		return err
+	}
+
+	filter := bson.M{
+		"_id":        classroomObjectID,
+		"rounds._id": roundObjectID,
+	}
+
+	update := bson.M{
+		"$push": bson.M{
+			"rounds.$.report_periods": bson.M{
+				"$each": reportPeriods,
+			},
+		},
+	}
+
+	res, err := p.classroomCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return err
+	}
+
+	if res.ModifiedCount == 0 {
+		return apperror.ErrRoundNotFound
+	}
+
+	return nil
+}
+
+func (p *projectRepo) GetReportPeriodByID(
+	ctx context.Context,
+	classroomID, roundID, reportPeriodID string,
+) (*model.ReportPeriod, error) {
+
+	classroomOID, err := primitive.ObjectIDFromHex(classroomID)
+	if err != nil {
+		return nil, err
+	}
+	roundOID, err := primitive.ObjectIDFromHex(roundID)
+	if err != nil {
+		return nil, err
+	}
+	reportPeriodOID, err := primitive.ObjectIDFromHex(reportPeriodID)
+	if err != nil {
+		return nil, err
+	}
+
+	pipeline := mongo.Pipeline{
+		{{Key: "$match", Value: bson.M{
+			"_id": classroomOID,
+		}}},
+		{{Key: "$unwind", Value: "$rounds"}},
+		{{Key: "$match", Value: bson.M{
+			"rounds._id": roundOID,
+		}}},
+		{{Key: "$unwind", Value: "$rounds.report_periods"}},
+		{{Key: "$match", Value: bson.M{
+			"rounds.report_periods._id": reportPeriodOID,
+		}}},
+		{{Key: "$replaceRoot", Value: bson.M{
+			"newRoot": "$rounds.report_periods",
+		}}},
+	}
+
+	cursor, err := p.classroomCollection.Aggregate(ctx, pipeline)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	if !cursor.Next(ctx) {
+		return nil, apperror.ErrNotFound
+	}
+
+	var period model.ReportPeriod
+	if err := cursor.Decode(&period); err != nil {
+		return nil, err
+	}
+
+	return &period, nil
+}
+
+func (p *projectRepo) GetReportPeriodsByRoundID(
+	ctx context.Context,
+	classroomID, roundID string,
+) ([]model.ReportPeriod, error) {
+
+	classroomOID, err := primitive.ObjectIDFromHex(classroomID)
+	if err != nil {
+		return nil, apperror.ErrBadRequest
+	}
+
+	roundOID, err := primitive.ObjectIDFromHex(roundID)
+	if err != nil {
+		return nil, apperror.ErrBadRequest
+	}
+
+	var result struct {
+		Rounds []struct {
+			ReportPeriods []model.ReportPeriod `bson:"report_periods"`
+		} `bson:"rounds"`
+	}
+
+	err = p.classroomCollection.FindOne(
+		ctx,
+		bson.M{"_id": classroomOID},
+		options.FindOne().SetProjection(bson.M{
+			"rounds": bson.M{
+				"$filter": bson.M{
+					"input": "$rounds",
+					"as":    "r",
+					"cond": bson.M{
+						"$eq": []interface{}{"$$r._id", roundOID},
+					},
+				},
+			},
+		}),
+	).Decode(&result)
+
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, apperror.ErrClassroomNotFound
+		}
+		return nil, err
+	}
+
+	if len(result.Rounds) == 0 {
+		return nil, apperror.ErrRoundNotFound
+	}
+
+	return result.Rounds[0].ReportPeriods, nil
 }
 
 func (p *projectRepo) ReplaceReportPeriod(ctx context.Context, reportPeriod *model.ReportPeriod) error {
--- backend/internal/service/project_service.go
+++ backend/internal/service/project_service.go
@@ -1,1 +1,511 @@
 package service
+
+import (
+	"time"
+
+	"github.com/giakiet05/quan-ly-do-an/backend/internal/apperror"
+	"github.com/giakiet05/quan-ly-do-an/backend/internal/dto"
+	"github.com/giakiet05/quan-ly-do-an/backend/internal/model"
+	"github.com/giakiet05/quan-ly-do-an/backend/internal/repo"
+	"github.com/giakiet05/quan-ly-do-an/backend/internal/util"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type ProjectService interface {
+	CreateProjectRound(req *dto.CreateProjectRoundRequest, requesterID string) (*model.ProjectRound, error)
+	CreateProjectRounds(req *dto.CreateProjectRoundsRequest, requesterID string) ([]model.ProjectRound, error)
+	GetProjectRoundByID(classroomID, roundID string) (*model.ProjectRound, error)
+	GetProjectRoundsByClassroomID(classroomID string) ([]model.ProjectRound, error)
+	UpdateProjectRound(req *dto.UpdateProjectRoundRequest, requesterID string) (*model.ProjectRound, error)
+	DeleteProjectRound(classroomID, roundID string, requesterID string) error
+
+	CreateProject(req *dto.CreateProjectRequest, requesterID string) (*model.Project, error)
+	CreateProjects(req *dto.CreateProjectsRequest, requesterID string) ([]model.Project, error)
+	GetProjectByID(classroomID string, projectID string) (*model.Project, error)
+	GetProjectsByRoundID(classroomID, roundID string) ([]model.Project, error)
+	UpdateProject(req *dto.UpdateProjectRequest, requesterID string) (*model.Project, error)
+	DeleteProject(classroomID, projectID string, requesterID string) error
+
+	CreateReportPeriod(req *dto.CreateReportPeriodRequest, classroomID, roundID, requesterID string) (*model.ReportPeriod, error)
+	CreateReportPeriods(req *dto.CreateReportPeriodsRequest, classroomID, requesterID string) ([]model.ReportPeriod, error)
+	GetReportPeriodByID(classroomID, roundID, reportPeriodID string) (*model.ReportPeriod, error)
+	GetReportPeriodsByRoundID(classroomID, roundID string) ([]model.ReportPeriod, error)
+	UpdateReportPeriod(req *dto.UpdateReportPeriodRequest, requesterID string) (*model.ReportPeriod, error)
+	DeleteReportPeriod(classroomID, roundID, reportPeriodID string, requesterID string) error
+}
+
+type projectService struct {
+	projectRepo   repo.ProjectRepo
+	classroomRepo repo.ClassroomRepo
+}
+
+func NewProjectService(projectRepo repo.ProjectRepo, classroomRepo repo.ClassroomRepo) ProjectService {
+	return &projectService{
+		projectRepo:   projectRepo,
+		classroomRepo: classroomRepo,
+	}
+}
+
+func (p *projectService) CreateProjectRound(req *dto.CreateProjectRoundRequest, requesterID string) (*model.ProjectRound, error) {
+	ctx, cancel := util.NewDefaultDBContext()
+	defer cancel()
+
+	isLecturer, err := p.classroomRepo.IsLecturer(ctx, req.ClassroomID, requesterID)
+	if err != nil {
+		return nil, err
+	}
+	if !isLecturer {
+		return nil, apperror.ErrForbidden
+	}
+
+	startDate, err := time.Parse("2006-01-02", req.StartDate)
+	if err != nil {
+		return nil, apperror.ErrBadRequest
+	}
+
+	endDate, err := time.Parse("2006-01-02", req.EndDate)
+	if err != nil {
+		return nil, apperror.ErrBadRequest
+	}
+
+	round := &model.ProjectRound{
+		Name:          req.Name,
+		StartDate:     startDate,
+		EndDate:       endDate,
+		Description:   req.Description,
+		Projects:      []model.Project{},
+		ReportPeriods: []model.ReportPeriod{},
+	}
+
+	err = p.projectRepo.CreateProjectRound(ctx, req.ClassroomID, round)
+	if err != nil {
+		return nil, err
+	}
+
+	return round, nil
+}
+
+func (p *projectService) CreateProjectRounds(req *dto.CreateProjectRoundsRequest, requesterID string) ([]model.ProjectRound, error) {
+	ctx, cancel := util.NewDefaultDBContext()
+	defer cancel()
+
+	isLecturer, err := p.classroomRepo.IsLecturer(ctx, req.ClassroomID, requesterID)
+	if err != nil {
+		return nil, err
+	}
+	if !isLecturer {
+		return nil, apperror.ErrForbidden
+	}
+
+	rounds := make([]model.ProjectRound, 0, len(req.Rounds))
+	for _, r := range req.Rounds {
+		startDate, err := time.Parse("2006-01-02", r.StartDate)
+		if err != nil {
+			return nil, apperror.ErrBadRequest
+		}
+
+		endDate, err := time.Parse("2006-01-02", r.EndDate)
+		if err != nil {
+			return nil, apperror.ErrBadRequest
+		}
+
+		rounds = append(rounds, model.ProjectRound{
+			Name:          r.Name,
+			StartDate:     startDate,
+			EndDate:       endDate,
+			Description:   r.Description,
+			Projects:      []model.Project{},
+			ReportPeriods: []model.ReportPeriod{},
+		})
+	}
+
+	err = p.projectRepo.CreateProjectRounds(ctx, req.ClassroomID, rounds)
+	if err != nil {
+		return nil, err
+	}
+	return rounds, nil
+}
+
+func (p *projectService) GetProjectRoundByID(classroomID, roundID string) (*model.ProjectRound, error) {
+	ctx, cancel := util.NewDefaultDBContext()
+	defer cancel()
+
+	return p.projectRepo.GetProjectRoundByID(ctx, classroomID, roundID)
+}
+
+func (p *projectService) GetProjectRoundsByClassroomID(classroomID string) ([]model.ProjectRound, error) {
+	ctx, cancel := util.NewDefaultDBContext()
+	defer cancel()
+
+	return p.projectRepo.GetProjectRoundsByClassroomID(ctx, classroomID)
+}
+
+func (p *projectService) UpdateProjectRound(req *dto.UpdateProjectRoundRequest, requesterID string) (*model.ProjectRound, error) {
+	ctx, cancel := util.NewDefaultDBContext()
+	defer cancel()
+
+	isLecturer, err := p.classroomRepo.IsLecturer(ctx, req.ClassroomID, requesterID)
+	if err != nil {
+		return nil, err
+	}
+	if !isLecturer {
+		return nil, apperror.ErrForbidden
+	}
+
+	round, err := p.projectRepo.GetProjectRoundByID(ctx, req.ClassroomID, req.ProjectRoundID)
+	if err != nil {
+		return nil, err
+	}
+
+	if req.Name != "" {
+		round.Name = req.Name
+	}
+	if req.StartDate != "" {
+		startDate, err := time.Parse("2006-01-02", req.StartDate)
+		if err != nil {
+			return nil, apperror.ErrBadRequest
+		}
+		round.StartDate = startDate
+	}
+	if req.EndDate != "" {
+		endDate, err := time.Parse("2006-01-02", req.EndDate)
+		if err != nil {
+			return nil, apperror.ErrBadRequest
+		}
+		round.EndDate = endDate
+	}
+	if req.Description != "" {
+		round.Description = req.Description
+	}
+
+	return round, nil
+}
+
+func (p *projectService) DeleteProjectRound(classroomID, roundID string, requesterID string) error {
+	ctx, cancel := util.NewDefaultDBContext()
+	defer cancel()
+
+	isLecturer, err := p.classroomRepo.IsLecturer(ctx, classroomID, requesterID)
+	if err != nil {
+		return err
+	}
+	if !isLecturer {
+		return apperror.ErrForbidden
+	}
+
+	return p.projectRepo.DeleteProjectRound(ctx, classroomID, roundID)
+}
+
+func (p *projectService) CreateProject(req *dto.CreateProjectRequest, requesterID string) (*model.Project, error) {
+	ctx, cancel := util.NewDefaultDBContext()
+	defer cancel()
+
+	isLecturer, err := p.classroomRepo.IsLecturer(ctx, req.ClassroomID, requesterID)
+	if err != nil {
+		return nil, err
+	}
+	if !isLecturer {
+		return nil, apperror.ErrForbidden
+	}
+
+	classroomOID, err := primitive.ObjectIDFromHex(req.ClassroomID)
+	if err != nil {
+		return nil, apperror.ErrInvalidID
+	}
+
+	roundOID, err := primitive.ObjectIDFromHex(req.ProjectRoundID)
+	if err != nil {
+		return nil, apperror.ErrInvalidID
+	}
+
+	project := &model.Project{
+		ID:             primitive.NewObjectID(),
+		ClassroomID:    classroomOID,
+		ProjectRoundID: roundOID,
+		Title:          req.Title,
+		Amount:         req.Amount,
+		Description:    req.Description,
+		MinMember:      req.MinMember,
+		MaxMember:      req.MaxMember,
+		Status:         model.ProjectStatusPending,
+	}
+
+	err = p.projectRepo.CreateProject(ctx, project)
+	if err != nil {
+		return nil, err
+	}
+
+	return project, nil
+}
+
+func (p *projectService) CreateProjects(req *dto.CreateProjectsRequest, requesterID string) ([]model.Project, error) {
+	ctx, cancel := util.NewDefaultDBContext()
+	defer cancel()
+
+	isLecturer, err := p.classroomRepo.IsLecturer(ctx, req.ClassroomID, requesterID)
+	if err != nil {
+		return nil, err
+	}
+	if !isLecturer {
+		return nil, apperror.ErrForbidden
+	}
+
+	classroomOID, err := primitive.ObjectIDFromHex(req.ClassroomID)
+	if err != nil {
+		return nil, apperror.ErrInvalidID
+	}
+
+	roundOID, err := primitive.ObjectIDFromHex(req.ProjectRoundID)
+	if err != nil {
+		return nil, apperror.ErrInvalidID
+	}
+
+	projects := make([]model.Project, 0, len(req.Projects))
+	for _, p := range req.Projects {
+		projects = append(projects, model.Project{
+			ID:             primitive.NewObjectID(),
+			ClassroomID:    classroomOID,
+			ProjectRoundID: roundOID,
+			Title:          p.Title,
+			Amount:         p.Amount,
+			Description:    p.Description,
+			MinMember:      p.MinMember,
+			MaxMember:      p.MaxMember,
+			Status:         model.ProjectStatusPending,
+		})
+	}
+
+	err = p.projectRepo.CreateProjects(ctx, projects)
+	if err != nil {
+		return nil, err
+	}
+	return projects, nil
+}
+
+func (p *projectService) GetProjectByID(classroomID string, projectID string) (*model.Project, error) {
+	ctx, cancel := util.NewDefaultDBContext()
+	defer cancel()
+
+	return p.projectRepo.GetProjectByID(ctx, classroomID, projectID)
+}
+
+func (p *projectService) GetProjectsByRoundID(classroomID, roundID string) ([]model.Project, error) {
+	ctx, cancel := util.NewDefaultDBContext()
+	defer cancel()
+
+	return p.projectRepo.GetProjectsByRoundID(ctx, classroomID, roundID)
+}
+
+func (p *projectService) UpdateProject(req *dto.UpdateProjectRequest, requesterID string) (*model.Project, error) {
+	ctx, cancel := util.NewDefaultDBContext()
+	defer cancel()
+
+	isLecturer, err := p.classroomRepo.IsLecturer(ctx, req.ClassroomID, requesterID)
+	if err != nil {
+		return nil, err
+	}
+	if !isLecturer {
+		return nil, apperror.ErrForbidden
+	}
+
+	project, err := p.projectRepo.GetProjectByID(ctx, req.ClassroomID, req.ProjectID)
+	if err != nil {
+		return nil, err
+	}
+
+	if req.Title != "" {
+		project.Title = req.Title
+	}
+	if req.Amount > 0 {
+		project.Amount = req.Amount
+	}
+	if req.Description != "" {
+		project.Description = req.Description
+	}
+	if req.MinMember > 0 {
+		project.MinMember = req.MinMember
+	}
+	if req.MaxMember > 0 {
+		project.MaxMember = req.MaxMember
+	}
+
+	err = p.projectRepo.ReplaceProject(ctx, project)
+	if err != nil {
+		return nil, err
+	}
+
+	return project, nil
+}
+
+func (p *projectService) DeleteProject(classroomID, projectID string, requesterID string) error {
+	ctx, cancel := util.NewDefaultDBContext()
+	defer cancel()
+
+	isLecturer, err := p.classroomRepo.IsLecturer(ctx, classroomID, requesterID)
+	if err != nil {
+		return err
+	}
+	if !isLecturer {
+		return apperror.ErrForbidden
+	}
+
+	return p.projectRepo.DeleteProject(ctx, classroomID, projectID)
+}
+
+func (p *projectService) CreateReportPeriod(req *dto.CreateReportPeriodRequest, classroomID, roundID, requesterID string) (*model.ReportPeriod, error) {
+	ctx, cancel := util.NewDefaultDBContext()
+	defer cancel()
+
+	isLecturer, err := p.classroomRepo.IsLecturer(ctx, classroomID, requesterID)
+	if err != nil {
+		return nil, err
+	}
+	if !isLecturer {
+		return nil, apperror.ErrForbidden
+	}
+
+	startDate, err := time.Parse("2006-01-02", req.StartDate)
+	if err != nil {
+		return nil, apperror.ErrBadRequest
+	}
+
+	endDate, err := time.Parse("2006-01-02", req.EndDate)
+	if err != nil {
+		return nil, apperror.ErrBadRequest
+	}
+
+	period := &model.ReportPeriod{
+		ID:          primitive.NewObjectID(),
+		Title:       req.Title,
+		Description: req.Description,
+		FileType:    req.FileType,
+		StartDate:   startDate,
+		EndDate:     endDate,
+	}
+
+	err = p.projectRepo.CreateReportPeriod(ctx, classroomID, roundID, period)
+	if err != nil {
+		return nil, err
+	}
+
+	return period, nil
+}
+
+func (p *projectService) CreateReportPeriods(req *dto.CreateReportPeriodsRequest, classroomID, requesterID string) ([]model.ReportPeriod, error) {
+	ctx, cancel := util.NewDefaultDBContext()
+	defer cancel()
+
+	isLecturer, err := p.classroomRepo.IsLecturer(ctx, classroomID, requesterID)
+	if err != nil {
+		return nil, err
+	}
+	if !isLecturer {
+		return nil, apperror.ErrForbidden
+	}
+
+	periods := make([]model.ReportPeriod, 0, len(req.ReportPeriods))
+	for _, p := range req.ReportPeriods {
+		startDate, err := time.Parse("2006-01-02", p.StartDate)
+		if err != nil {
+			return nil, apperror.ErrBadRequest
+		}
+
+		endDate, err := time.Parse("2006-01-02", p.EndDate)
+		if err != nil {
+			return nil, apperror.ErrBadRequest
+		}
+
+		periods = append(periods, model.ReportPeriod{
+			ID:          primitive.NewObjectID(),
+			Title:       p.Title,
+			Description: p.Description,
+			FileType:    p.FileType,
+			StartDate:   startDate,
+			EndDate:     endDate,
+		})
+	}
+
+	err = p.projectRepo.CreateReportPeriods(ctx, classroomID, req.ProjectRoundID, periods)
+	if err != nil {
+		return nil, err
+	}
+	return periods, nil
+}
+
+func (p *projectService) GetReportPeriodByID(classroomID, roundID, reportPeriodID string) (*model.ReportPeriod, error) {
+	ctx, cancel := util.NewDefaultDBContext()
+	defer cancel()
+
+	return p.projectRepo.GetReportPeriodByID(ctx, classroomID, roundID, reportPeriodID)
+}
+
+func (p *projectService) GetReportPeriodsByRoundID(classroomID, roundID string) ([]model.ReportPeriod, error) {
+	ctx, cancel := util.NewDefaultDBContext()
+	defer cancel()
+
+	return p.projectRepo.GetReportPeriodsByRoundID(ctx, classroomID, roundID)
+}
+
+func (p *projectService) UpdateReportPeriod(req *dto.UpdateReportPeriodRequest, requesterID string) (*model.ReportPeriod, error) {
+	ctx, cancel := util.NewDefaultDBContext()
+	defer cancel()
+
+	isLecturer, err := p.classroomRepo.IsLecturer(ctx, req.ClassroomID, requesterID)
+	if err != nil {
+		return nil, err
+	}
+	if !isLecturer {
+		return nil, apperror.ErrForbidden
+	}
+
+	period, err := p.projectRepo.GetReportPeriodByID(ctx, req.ClassroomID, req.ProjectRoundID, req.ReportPeriodID)
+	if err != nil {
+		return nil, err
+	}
+
+	if req.Title != "" {
+		period.Title = req.Title
+	}
+	if req.Description != "" {
+		period.Description = req.Description
+	}
+	if len(req.FileType) > 0 {
+		period.FileType = req.FileType
+	}
+	if req.StartDate != "" {
+		startDate, err := time.Parse("2006-01-02", req.StartDate)
+		if err != nil {
+			return nil, apperror.ErrBadRequest
+		}
+		period.StartDate = startDate
+	}
+	if req.EndDate != "" {
+		endDate, err := time.Parse("2006-01-02", req.EndDate)
+		if err != nil {
+			return nil, apperror.ErrBadRequest
+		}
+		period.EndDate = endDate
+	}
+
+	err = p.projectRepo.ReplaceReportPeriod(ctx, period)
+	if err != nil {
+		return nil, err
+	}
+
+	return period, nil
+}
+
+func (p *projectService) DeleteReportPeriod(classroomID, roundID, reportPeriodID string, requesterID string) error {
+	ctx, cancel := util.NewDefaultDBContext()
+	defer cancel()
+
+	isLecturer, err := p.classroomRepo.IsLecturer(ctx, classroomID, requesterID)
+	if err != nil {
+		return err
+	}
+	if !isLecturer {
+		return apperror.ErrForbidden
+	}
+
+	return p.projectRepo.DeleteReportPeriod(ctx, classroomID, roundID, reportPeriodID)
+}
