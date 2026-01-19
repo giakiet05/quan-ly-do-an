15bab3caa8edda336d412826244ff969cc8304b3
Thanh456 <pmtcyty@gmail.com>
2026-01-19 22:42:56 +0700

    merge dev

--- backend/internal/bootstrap/init.go
+++ backend/internal/bootstrap/init.go
@@ -99,16 +99,6 @@ func initServices(
 		ClassroomInvitationService: service.NewClassroomInvitationService(repos.ClassroomInvitationRepo, repos.ClassroomRepo, repos.UserRepo, eventBus),
 		ClassPostService:           service.NewClassPostService(repos.ClassPostRepo, repos.ClassroomRepo, repos.UserRepo),
 		ProjectService:             service.NewProjectService(repos.ProjectRepo, repos.ClassroomRepo, repos.GroupRepo, eventBus, cron),
-		GroupService:               service.NewGroupService(repos.GroupRepo, repos.ClassroomRepo, repos.ChannelRepo, repos.UserRepo, repos.ProjectRepo),
-		AuthService:                service.NewAuthService(repos.UserRepo, repos.EmailVerificationRepo, repos.PasswordResetRepo, emailSender, redisClient, tokenService),
-		UserService:                service.NewUserService(repos.UserRepo, eventBus, redisClient),
-		NotificationService:        service.NewNotificationService(repos.NotificationRepo, repos.UserRepo, eventBus, redisClient),
-		ChannelService:             service.NewChannelService(repos.ChannelRepo, repos.MessageRepo, eventBus),
-		MessageService:             service.NewMessageService(repos.MessageRepo, repos.ChannelRepo, eventBus, redisClient),
-		ClassroomJoinService:       service.NewClassroomJoinService(repos.ClassroomJoinRequestRepo, repos.ClassroomRepo, repos.UserRepo),
-		ClassroomService:           service.NewClassroomService(repos.ClassroomRepo, repos.UserRepo, repos.ChannelRepo),
-		ClassPostService:           service.NewClassPostService(repos.ClassPostRepo, repos.ClassroomRepo, repos.UserRepo),
-		ProjectService:             service.NewProjectService(repos.ProjectRepo, repos.ClassroomRepo),
 	}
 }
 
