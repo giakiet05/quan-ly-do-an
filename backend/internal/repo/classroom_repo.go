85c41fa7be1ede46a5367ecf3963a3f40553a9ea
giakiet05 <giakiet05@gmail.com>
2026-01-20 17:50:03 +0700

    feat: add error check for some repo function

--- backend/internal/repo/classroom_repo.go
+++ backend/internal/repo/classroom_repo.go
@@ -193,7 +193,9 @@ func (c *classroomRepo) GetByLecturer(ctx context.Context, lecturerID string, pa
 	defer cursor.Close(ctx)
 
 	var classrooms []model.Classroom
-	cursor.All(ctx, &classrooms)
+	if err := cursor.All(ctx, &classrooms); err != nil {
+		return nil, 0, err
+	}
 
 	return classrooms, total, nil
 }
@@ -232,7 +234,9 @@ func (c *classroomRepo) GetByStudent(ctx context.Context, studentID string, page
 	defer cursor.Close(ctx)
 
 	var classrooms []model.Classroom
-	cursor.All(ctx, &classrooms)
+	if err := cursor.All(ctx, &classrooms); err != nil {
+		return nil, 0, err
+	}
 
 	return classrooms, total, nil
 }
